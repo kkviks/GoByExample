6a92c3cdca3e5ff62a536ce2355d78138389e54c
kkviks <kkviks@gmail.com>
2021-09-17 18:55:36 +0530

    Add methods

---
+++ main/methods.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perimeter() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+	r := rect{10, 5}
+
+	fmt.Println(r.area())
+	fmt.Println(r.perimeter())
+}
